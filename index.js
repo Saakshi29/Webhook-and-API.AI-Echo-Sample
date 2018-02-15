e66641411f6f04236a2b0ab52856f7ce5d27c250
Saakshi <saakshi.gupta2910@gmail.com>
2018-02-15 12:20:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
-
+/*
 restService.post("/", function(req, res) {
   var speech ='https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow';
  
@@ -21,7 +21,26 @@ restService.post("/", function(req, res) {
     displayText: speech,
     source: "webhook-echo-sample"
   });
+});*/
+
+restService.post("/", function(req, res) {
+  var speech ='https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow';
+ 
+  return res.json({
+    "buttons": [
+      {
+        "postback": "https://github.com/dialogflow/dialogflow-nodejs-client-v2",
+        "text": "Card Link Title"
+      }
+    ],
+    "imageUrl": "",
+    "platform": "slack",
+    "subtitle": "hello",
+    "title": "hello",
+    "type": 1
+  });
 });
+
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
