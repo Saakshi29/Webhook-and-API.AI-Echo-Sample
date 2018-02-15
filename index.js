e0f901154cf69d224161219cbe91b990d14c6989
Saakshi <saakshi.gupta2910@gmail.com>
2018-02-15 12:15:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,14 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
-/*
+
 restService.post("/", function(req, res) {
-  var speech =
-    req.body.result.metadata.intentName ;
+  var speech ='https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow';
+ 
   return res.json({
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
-  });
-});*/
-restService.post("/", function(req, res) {
-  var speech =
-    req.body.result.metadata.intentName ;
-  return res.json({
-    "buttons": [
-      {
-        "postback":"https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow",
-        "text": "Card Link Title"
-      }
-    ],
-    "imageUrl": "http://urltoimage.com",
-    "platform": "slack",
-    "subtitle": "Card Subtitle",
-    "title": "Card Title",
-    "type": 1
   });
 });
 
