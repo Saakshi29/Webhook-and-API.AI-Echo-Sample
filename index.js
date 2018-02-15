8d379e67db30bd3f0bf8252a8a796ad9ccc44861
Saakshi <saakshi.gupta2910@gmail.com>
2018-02-15 12:07:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
-
+/*
 restService.post("/", function(req, res) {
   var speech =
     req.body.result.metadata.intentName ;
@@ -21,8 +21,24 @@ restService.post("/", function(req, res) {
     displayText: speech,
     source: "webhook-echo-sample"
   });
+});*/
+restService.post("/", function(req, res) {
+  var speech =
+    req.body.result.metadata.intentName ;
+  return res.json({
+    "buttons": [
+      {
+        "postback":"https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow",
+        "text": "Card Link Title"
+      }
+    ],
+    "imageUrl": "http://urltoimage.com",
+    "platform": "facebook",
+    "subtitle": "Card Subtitle",
+    "title": "Card Title",
+    "type": 1
+  });
 });
-
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
