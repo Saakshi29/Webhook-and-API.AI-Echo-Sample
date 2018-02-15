ffbda1758a01f684f9b764c93e4055f7d79b6579
Saakshi <saakshi.gupta2910@gmail.com>
2018-02-15 22:41:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,93 +16,12 @@ restService.use(bodyParser.json());
 restService.post("/", function(req, res) {
   var speech ='https://www.youtube.com/results?search_query=sending+pdf+and+links+in+response+of+diologflow';
  
+  
+  
   return res.json({
-   {
-  "speech": "",
-  "displayText": "",
-  "data": {
-
-  },
-  "contextOut": [
-
-  ],
+  "speech": "hello anevdnu gupta here",
+  "displayText": "hello anvendu gupta herere",
   "source": "Webhook",
-  "messages": [
-    {
-      "type": 4,
-      "platform": "skype",
-      "speech": "",
-      "payload": {
-        "skype": {
-          "attachmentLayout": "list",
-          "attachments": [
-            {
-              "contentType": "application\/vnd.microsoft.card.hero",
-              "content": {
-                "title": "Unit 2A",
-                "subtitle": "",
-                "text": "These timeslots are available for 2017-10-16",
-                "images": [
-
-                ],
-                "buttons": [
-                  {
-                    "type": "imBack",
-                    "title": "from 13:00 until 14:00 Unit 2A",
-                    "value": "from 13:00 until 14:00 Unit 2A"
-                  },
-                  {
-                    "type": "imBack",
-                    "title": "from 14:00 until 15:00 Unit 2A",
-                    "value": "from 14:00 until 15:00 Unit 2A"
-                  },
-                  {
-                    "type": "imBack",
-                    "title": "from 15:00 until 16:00 Unit 2A",
-                    "value": "from 15:00 until 16:00 Unit 2A"
-                  }
-                ]
-              }
-            },
-            {
-              "contentType": "application\/vnd.microsoft.card.hero",
-              "content": {
-                "title": "Unit 1",
-                "subtitle": "",
-                "text": "These timeslots are available for 2017-10-16",
-                "images": [
-
-                ],
-                "buttons": [
-                  {
-                    "type": "imBack",
-                    "title": "from 13:00 until 14:00 Unit 1",
-                    "value": "from 13:00 until 14:00 Unit 1"
-                  },
-                  {
-                    "type": "imBack",
-                    "title": "from 14:00 until 15:00 Unit 1",
-                    "value": "from 14:00 until 15:00 Unit 1"
-                  },
-                  {
-                    "type": "imBack",
-                    "title": "from 15:00 until 16:00 Unit 1",
-                    "value": "from 15:00 until 16:00 Unit 1"
-                  },
-                  {
-                    "type": "imBack",
-                    "title": "from 16:00 until 17:00 Unit 1",
-                    "value": "from 16:00 until 17:00 Unit 1"
-                  }
-                ]
-              }
-            }
-          ]
-        }
-      }
-    }
-  ]
-}
 
   });
 });
